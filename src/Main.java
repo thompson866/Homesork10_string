64b47918c8994b1a75739c450d33c7956162c4ba
arxip <arxipov1989@gmail.com>
2023-01-25 11:58:12 +0300

    домашка строки

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String firstName = "Ivan ";
+        String middleName ="Ivanovich " ;
+        String lastName = "Ivanov ";
+        String fulltName = lastName+firstName+middleName;
+        System.out.println("ФИО сотрудника — "+ fulltName);
+    }
+}
