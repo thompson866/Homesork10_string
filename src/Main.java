183cbde88d2452aca53fbc6c5973072943210931
arxip <arxipov1989@gmail.com>
2023-01-25 14:20:29 +0300

    домашка строки

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
+        task3();
 
     }
 
@@ -10,6 +12,20 @@ public class Main {
         String lastName = "Ivanov ";
         String fulltName = lastName + firstName + middleName;
         System.out.println("ФИО сотрудника — " + fulltName);
+        //task 2 test
+        //System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fulltName.toUpperCase());
+    }
+    public static void task2() {
+        String fulltName = "Ivanov Ivan Ivanovich";
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fulltName.toUpperCase());
+
+
+    }
+    public static void task3() {
+        String fulltName = "Иванов Семён Семёнович";
+        fulltName = fulltName.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника — " + fulltName  );
+
 
     }
 }
